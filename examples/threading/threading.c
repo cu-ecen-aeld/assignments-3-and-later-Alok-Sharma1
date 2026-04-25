cb228f9843668a2a84c4cc5136f532a7bb74fc50
vboxuser <vboxuser@Ubuntu22.myguest.virtualbox.org>
2026-04-25 20:52:17 +0530

    Assignment 4 Part 1: Implemented pthreads and mutex locking

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,26 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    int rc = pthread_mutex_lock(thread_func_args->mutex);
+    if(rc != 0){
+        ERROR_LOG("Failed to obtain mutex");
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+
+    rc = pthread_mutex_unlock(thread_func_args->mutex);
+    if (rc != 0) {
+        ERROR_LOG("Failed to unlock mutex! Error code: %d", rc);
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    // 5. If everything succeeded, set the success flag to true
+    thread_func_args->thread_complete_success = true;
+
     return thread_param;
 }
 
@@ -28,6 +48,30 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    // 1. Dynamically allocate memory for the thread data structure
+    struct thread_data *data = (struct thread_data *)malloc(sizeof(struct thread_data));
+    if (data == NULL) {
+        ERROR_LOG("Failed to allocate memory for thread_data");
+        return false;
+    }
+
+    // 2. Setup mutex and wait arguments
+    data->mutex = mutex;
+    data->wait_to_obtain_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+    data->thread_complete_success = false; // Will be set to true by threadfunc upon success
+
+    // 3. Pass thread_data to created thread using threadfunc() as entry point
+    int rc = pthread_create(thread, NULL, threadfunc, data);
+    
+    if (rc != 0) {
+        ERROR_LOG("Failed to create thread! Error code: %d", rc);
+        // Free the memory if thread creation fails to avoid memory leaks
+        free(data);
+        return false;
+    }
+
+    // Return true if successful
+    return true;
 }
 
